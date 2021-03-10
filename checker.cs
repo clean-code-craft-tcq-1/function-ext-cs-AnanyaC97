600d4b3e1d739df7c98afe30d4f0e862736ecf4e
Ananya Chandramohan <77439415+AnanyaC97@users.noreply.github.com>
2021-03-10 18:57:46 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -1,56 +1,31 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 
 namespace BatteryManagement
 {
     public class BatteryCheckerFactors
     {
-        public static string Language;
         public static bool batteryIsOk(float Temperature, float StateOfCharge, float ChargeRate, string Language)
         {
             bool optimumTemperatureLimit = BatteryTemperature.CheckTemperature(Temperature, Language);
             bool optimumStateOfCharge = BatteryStateOfCharge.CheckStateOfCharge(StateOfCharge, Language);
             bool optimumChargeRate = BatteryChargeRate.CheckChargeRate(ChargeRate, Language);
-
             if (optimumTemperatureLimit && optimumStateOfCharge && optimumChargeRate)
             {
-                Console.WriteLine("Battery is ok!");
-                return true;
+                Console.WriteLine("Battery is ok!\nDie Batterie ist ok!\n"); return true;
             }
             else
             {
-                Console.WriteLine("Battery is faulty!");
-                return false;
+                Console.WriteLine("Battery is faulty!\nBatterie ist fehlerhaft!\n"); return false;
             }
-        }
-        public static string LanguageInput()
-        {
-            Console.WriteLine("Please select language: \n1.English\n2.German \n");
-            int LanguageChoice = int.Parse(Console.ReadLine());
-            switch(LanguageChoice)
-            {
-                case 1:
-                    Language = "English";
-                    break;
-                case 2:
-                    Language = "German";
-                    break;
-                default:
-                    Language = "Wrong Input";
-                    Environment.Exit(0);
-                    break;
-            }
-            return Language;
         }
         public static int Main()
         {
-            Language = LanguageInput();
-
-            BatteryStatusDisplay.ExpectTrue(batteryIsOk(25, 70, 0.7f, Language));
-            BatteryStatusDisplay.ExpectFalse(batteryIsOk(50, 85, 0.0f, Language));
-            BatteryStatusDisplay.ExpectFalse(batteryIsOk(-50, 10, 0.9f, Language));
-            BatteryStatusDisplay.ExpectFalse(batteryIsOk(30, 100, 0.0f, Language));
-
+            BatteryStatusDisplay.ExpectTrue(batteryIsOk(25, 70, 0.7f, "English"));
+            BatteryStatusDisplay.ExpectTrue(batteryIsOk(25, 70, 0.7f, "German"));
+            BatteryStatusDisplay.ExpectFalse(batteryIsOk(50, 85, 0.0f, "English"));
+            BatteryStatusDisplay.ExpectFalse(batteryIsOk(-50, 10, 0.9f, "German"));
+            BatteryStatusDisplay.ExpectFalse(batteryIsOk(30, 100, 0.0f, "English"));
             return 0;
         }
     }
