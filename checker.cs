b9f462a1cdd71808a0a390eb7ed08ffd1bd0caad
Ananya Chandramohan <77439415+AnanyaC97@users.noreply.github.com>
2021-03-21 21:40:14 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -1,51 +1,39 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 
 namespace BatteryManagement
 {
     public class BatteryCheckerFactors
     {
-        public static void BatteryChecker(float Temperature, float StateOfCharge, float ChargeRate, string language)
+        public static bool BatteryStatus;
+        public static bool BatteryChecker(float Temperature, float StateOfCharge, float ChargeRate, string language)
         {
             bool optimumTemperatureLimit = BatteryTemperature.CheckTemperature(Temperature, language);
             bool optimumStateOfCharge = BatteryStateOfCharge.CheckStateOfCharge(StateOfCharge, language);
             bool optimumChargeRate = BatteryChargeRate.CheckChargeRate(ChargeRate, language);
             BatteryConsolidate.DisplayBatteryStatus(language);
-            bool BatteryStatus = optimumTemperatureLimit && optimumStateOfCharge && optimumChargeRate;
+            return optimumTemperatureLimit && optimumStateOfCharge && optimumChargeRate;
+        }
+        public static void DisplayBatteryStatusGerman(bool BatteryStatus)
+        {
             if (BatteryStatus)
-                BatteryOk(language);
+                Console.WriteLine(BatteryStatusDisplay.German.BatteryOK + "\n");
             else
-                BatteryFaulty(language);
+                Console.WriteLine(BatteryStatusDisplay.German.BatteryFaulty + "\n");
         }
-        public static void DisplayMessageGerman(object BatteryState)
+        public static void DisplayBatteryStatusEnglish(bool BatteryStatus)
         {
-            Console.WriteLine(BatteryState + "\n");
-        }
-
-        public static void DisplayMessageEnglish(object BatteryState)
-        {
-            Console.WriteLine(BatteryState + "\n");
-        }
-        public static void BatteryFaulty(string language)
-        {
-            if (language == "German")
-                DisplayMessageGerman(BatteryStatusDisplay.German.BatteryFaulty);
-            if (language == "English")
-                DisplayMessageEnglish(BatteryStatusDisplay.English.BatteryFaulty);
-        }
-        public static void BatteryOk(string language)
-        {
-            if (language == "German")
-                DisplayMessageGerman(BatteryStatusDisplay.German.BatteryOK);
-            if (language == "English")
-                DisplayMessageEnglish(BatteryStatusDisplay.English.BatteryOK);
+            if (BatteryStatus)
+                Console.WriteLine(BatteryStatusDisplay.English.BatteryOK + "\n");
+            else
+                Console.WriteLine(BatteryStatusDisplay.English.BatteryFaulty + "\n");
         }
         public static int Main()
         {
-            BatteryChecker(25, 70, 0.7f, "English");
-            BatteryChecker(1, 19, 0.0f, "English");
-            BatteryChecker(30, 100, 0.0f, "German");
-            BatteryChecker(-50, 10, 0.9f, "German");
+            DisplayBatteryStatusEnglish(BatteryChecker(25, 70, 0.7f, "English"));
+            DisplayBatteryStatusEnglish(BatteryChecker(1, 19, 0.0f, "English"));
+            DisplayBatteryStatusGerman(BatteryChecker(30, 100, 0.0f, "German"));
+            DisplayBatteryStatusGerman(BatteryChecker(-50, 10, 0.9f, "German"));
             return 0;
         }
     }
